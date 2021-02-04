49d52c29bc2208ccf9808e091fb51e588e4daebe
sametsolaklar <61630850+sametsolaklar@users.noreply.github.com>
2021-02-04 19:39:27 +0300

    ReCapProject

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car car)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var addedCar = context.Entry(car);
+                addedCar.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var deleteCar = context.Entry(car);
+                deleteCar.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+
+        public List<Car> GetAll(Expression<Func<Car, bool>>filter = null)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+
+        public void Update(Car car)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var updateCar = context.Entry(car);
+                updateCar.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
